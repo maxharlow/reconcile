83ed1191e91729528d869d15b511adef7053b421
Max Harlow <maxharlow@gmail.com>
2016-05-26 20:35:25 +0100

    Deal with multiple responses, pass over errors, use Promises, etc

--- reconcile-individual-names-to-company-officer-names.js
+++ reconcile-individual-names-to-company-officer-names.js
@@ -1,24 +1,38 @@
-const Stream = require('stream')
 const Highland = require('highland')
 const Request = require('request')
 
 const http = Highland.wrapCallback((location, callback) => {
     Request(location, (error, response) => {
-        const failure = error ? error : (response.statusCode >= 400) ? new Error(response.statusCode) : null
-        callback(failure, JSON.parse(response.body))
+        const jurisdiction = location.query.individualJuristiction ? ' (' + location.query.individualJuristiction + ')' : ''
+        const failure = error ? error
+              : response.statusCode >=  400 ? new Error('Error ' + response.statusCode + ': ' + location.query.individualName + ' (' + juristiction + ')')
+              : null
+        callback(failure, response)
     })
 })
 
 function locate(entry) {
     const apiVersion = 'v0.4.5'
-    return 'https://api.opencorporates.com/' + apiVersion + '/officers/search'
+    const location = 'https://api.opencorporates.com/' + apiVersion + '/officers/search'
         + '?q=' + entry.individualName
         + (entry.individualJuristiction ? '&jurisdiction_code=' + entry.individualJuristiction : '')
         + (entry.apiToken ? '&api_token=' + entry.apiToken : '')
+    return {
+        uri: location,
+        query: {
+            individualName: entry.individualName,
+            individualJuristiction: entry.individualJuristiction
+        }
+    }
 }
 
 function parse(response) {
-    return response.results.officers.map(officer => {
+    const body = JSON.parse(response.body)
+    if (body.results.officers.length === 0) {
+        const jurisdiction = response.request.query.individualJuristiction ? ' (' + response.request.query.individualJuristiction + ')' : ''
+        throw new Error('Individual not found: ' + response.request.query.individualName + jurisdiction)
+    }
+    return body.results.officers.map(officer => {
 	    return {
 	        officerName: officer.officer.name,
 	        officerPosition: officer.officer.position,
@@ -28,20 +42,15 @@ function parse(response) {
     })
 }
 
-function execute(input, output) {
-    Highland([input])
-        .map(locate)
-        .flatMap(http)
-        .flatMap(parse)
-        .collect()
-        .each(output)
-}
-
-const run = new Stream.Transform({ objectMode: true })
-run._transform = (chunk, _, done) => {
-    execute(chunk, data => {
-        run.push(data)
-        done()
+function run(input) {
+    return new Promise((resolve, reject) => {
+        Highland([input])
+            .map(locate)
+            .flatMap(http)
+            .flatMap(parse)
+            .collect()
+            .errors(reject)
+            .each(resolve)
     })
 }
 
