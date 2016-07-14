bc79995cb815b604746425471ec0ba0ece83ca0b
Max Harlow <maxharlow@gmail.com>
2016-07-14 13:56:49 +0100

    More lenient parameter handling

--- reconcile.js
+++ reconcile.js
@@ -2,6 +2,7 @@ const FS = require('fs')
 const Process = require('process')
 const Yargs = require('yargs')
 const Highland = require('highland')
+const Yaml = require('js-yaml')
 const CSVParser = require('csv-parser')
 const CSVWriter = require('csv-write-stream')
 
@@ -14,23 +15,41 @@ function setup() {
         .wrap(null)
         .demand(1, '')
         .completion('completion', false, () => reconcilers)
-        .option('p', { alias: 'parameters', type: 'string', describe: 'Json configuration object to be passed to the reconciler' })
+        .option('p', { alias: 'parameters', type: 'string', describe: 'Parameters to be passed to the reconciler, either in Json or Yaml' })
         .help('?').alias('?', 'help')
         .version().alias('v', 'version')
     reconcilers.forEach(reconciler => interface.command(reconciler))
     if (interface.argv['get-yargs-completions']) Process.exit(0)
-    else run(interface.argv._[0], interface.argv._[1], interface.argv.parameters)
+    else run(interface.argv._[0], interface.argv._[1], parse(interface.argv.parameters))
+}
+
+function parse(input) {
+    if (!input) return {}
+    try {
+        const file = FS.readFileSync(input, 'utf8')
+        const data = Yaml.load(file)
+        return data
+    }
+    catch (e) { // it's not a file, try and parse it instead
+        try {
+            const data = Yaml.load(input)
+            return data
+        }
+        catch (e) {
+            console.error(e.message)
+            Process.exit(1)
+        }
+    }
 }
 
 function combine(fn) {
     return input => fn(input).then(output => output.map(outputRow => Object.assign({}, input, outputRow)))
 }
 
-function run(command, input, parametersData) {
+function run(command, input, parameters) {
     const filename = input === undefined || input === '-' ? '/dev/stdin' : input
     try {
         if (Process.stdin.isTTY === true && filename === '/dev/stdin') throw new Error('Error: no input')
-        const parameters = parametersData ? JSON.parse(parametersData) : {}
         const reconciler = require('./reconcile-' + command)(parameters)
         const reconcilerCombined = item => Highland(combine(reconciler)(item))
         Highland(Highland.wrapCallback(FS.readFile)(filename))
