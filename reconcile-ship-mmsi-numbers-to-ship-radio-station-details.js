e8a6c1dc6076df99a7992b100623aa66ee4a0cf2
Max Harlow <maxharlow@gmail.com>
2019-12-20 10:48:58 +0000

    Add ITU lookup

---
+++ reconcile-ship-mmsi-numbers-to-ship-radio-station-details.js
@@ -0,0 +1,82 @@
+const Querystring = require('querystring')
+const Cheerio = require('cheerio')
+
+function initialise(parameters, requestor) {
+
+    const request = requestor(Infinity, e => {
+        const ship = e.config.passthrough.shipMMSINumber
+        if (e.response.status >= 400) return `Received code ${e.response.status} for ship ${ship}`
+    })
+
+    function locate(entry, key) {
+        const shipMMSINumber = entry[parameters.shipMMSINumberField || 'shipMMSINumber']
+        if (!shipMMSINumber) throw new Error('No ship MMSI number found')
+        return {
+            url: 'https://www.itu.int/mmsapp/ShipStation/list',
+            method: 'POST',
+            data: Querystring.stringify({
+                'Search.MaritimeMobileServiceIdentity': shipMMSINumber,
+                viewCommand: 'Search'
+            }),
+            passthrough: {
+                shipMMSINumber
+            }
+        }
+    }
+
+    function details(response) {
+        const document = Cheerio.load(response.data)
+        return {
+            url: 'https://www.itu.int/mmsapp/ShipStation/list',
+            method: 'POST',
+            data: Querystring.stringify({
+                onview: document('[title="View Ship Station"]').attr('value')
+            }),
+            passthrough: {
+                shipMMSINumber: response.passthrough.shipMMSINumber
+            }
+        }
+    }
+
+    function parse(response) {
+        const document = Cheerio.load(response.data)
+        return {
+            shipName: document('label').eq(0).text().trim(),
+            shipCallSign: document('label').eq(1).text().trim(),
+            shipIdentificationNumber: document('label').eq(22).text().trim(),
+            shipOwner: document('label').eq(18).text().trim(),
+            shipFormerName: document('label').eq(19).text().trim(),
+            shipTonnage: document('label').eq(23).text().trim(),
+            shipPersonCapacity: document('label').eq(24).text().trim()
+        }
+    }
+
+    async function run(input) {
+        const dataLocated = locate(input)
+        const dataLocatedRequested = await request(dataLocated)
+        const dataDetailed = details(dataLocatedRequested)
+        const dataDetailedRequested = await request(dataDetailed)
+        const dataParsed = parse(dataDetailedRequested)
+        return dataParsed
+    }
+
+    return run
+
+}
+
+const details = {
+    parameters: [
+        { name: 'shipMMSINumberField', description: 'Ship MMSI number field. [optional, default: "shipMMSINumber"]' }
+    ],
+    columns: [
+        { name: 'shipName' },
+        { name: 'shipCallSign' },
+        { name: 'shipIdentificationNumber' },
+        { name: 'shipOwner' },
+        { name: 'shipFormerName' },
+        { name: 'shipTonnage' },
+        { name: 'shipPersonCapacity' }
+    ]
+}
+
+module.exports = { initialise, details }
