c3158bcc603c57af34342f24ee06c236ec0fe6ff
Max Harlow <maxharlow@gmail.com>
2021-01-25 17:53:36 +0000

    Use optional chaining

--- reconcilers/uk-company-numbers-to-charges.js
+++ reconcilers/uk-company-numbers-to-charges.js
@@ -57,47 +57,47 @@ function initialise(parameters, requestor, die) {
         const charges = response.data.items
         return charges.map(charge => {
             const fields = {
-                chargeCode: charge.charge_code ? charge.charge_code : null,
-                chargeNumber: charge.charge_number ? charge.charge_number : null,
+                chargeCode: charge.charge_code || null,
+                chargeNumber: charge.charge_number || null,
                 chargeStatus: charge.status,
                 chargePersonsEntitled: charge.persons_entitled.map(person => person.name).join('; '),
-                chargeAcquiredDate: charge.acquired_on ? charge.acquired_on : null,
-                chargeCreatedDate: charge.created_on ? charge.created_on : null,
-                chargeDeliveredDate: charge.delivered_on ? charge.delivered_on : null,
-                chargeResolvedDate: charge.resolved_on ? charge.resolved_on : null,
-                chargeSatisfiedDate: charge.satisfied_on ? charge.satisfied_on : null,
-                chargeCoveringInstrumentDate: charge.covering_instrument_date ? charge.covering_instrument_date : null,
-                chargeTransactions: charge.transactions ? charge.transactions.map((transaction, i) => {
+                chargeAcquiredDate: charge.acquired_on || null,
+                chargeCreatedDate: charge.created_on || null,
+                chargeDeliveredDate: charge.delivered_on || null,
+                chargeResolvedDate: charge.resolved_on || null,
+                chargeSatisfiedDate: charge.satisfied_on || null,
+                chargeCoveringInstrumentDate: charge.covering_instrument_date || null,
+                chargeTransactions: charge.transactions?.map((transaction, i) => {
                     return `#${i + 1}`
                         + (transaction.filing_type ? ` ${transaction.filing_type}` : '')
                         + (transaction.links && transaction.links.filing ? `: https://beta.companieshouse.gov.uk${transaction.links.filing}/document` : '')
                         + (transaction.delivered_on ? ` (${transaction.delivered_on})` : '')
                         + (transaction.transaction_id ? ` [transaction ${transaction.transaction_id}]` : '')
                         + (transaction.insolvency_case_number || (transaction.links && transaction.links.insolvency_case) ? ' [insolvency case' : '')
-                        + (transaction.insolvency_case_number ? ` ${insolvency_case_number}` : '')
+                        + (transaction.insolvency_case_number ? ` ${transaction.insolvency_case_number}` : '')
                         + (transaction.links && transaction.links.insolvency_case ? ` ${transaction.links.insolvency_case}` : '')
                         + (transaction.insolvency_case_number || (transaction.links && transaction.links.insolvency_case) ? ']' : '')
-                }).join('; ') : null,
-                chargeInsolvencyCases: charge.insolvency_cases ? charge.insolvency_cases.map((insolvencyCase, i) => {
+                }).join('; ') || null,
+                chargeInsolvencyCases: charge.insolvency_cases?.map((insolvencyCase, i) => {
                     return `#${i + 1}`
                         + (insolvencyCase.links && insolvencyCase.links.case ? `: insolvencyCase.links.case` : '')
                         + (insolvencyCase.case_number ? ` [case ${insolvencyCase.case_number}]`: '')
                         + (insolvencyCase.transaction_id ? ` [transaction ${insolvencyCase.transaction_id}]` : '')
-                }) : null,
-                chargeParticulars: charge.particulars.description || null,
-                chargeContainsFixedCharge: charge.particulars.contains_fixed_charge || false,
-                chargeContainsFloatingCharge: charge.particulars.contains_floating_charge || false,
-                chargeContainsNegativePledge: charge.particulars.contains_negative_pledge || false,
-                chargeFloatingChargeCoversAll: charge.particulars.floating_charge_covers_all || false,
-                chargeChargorActingAsBareTrustee: charge.particulars.chargor_acting_as_bare_trustee || false,
-                chargeHasScottishAlterationsToOrder: charge.scottish_alterations && charge.scottish_alterations.has_alterations_to_order ? charge.scottish_alterations.has_alterations_to_order : false,
-                chargeHasScottishAlterationsToProhibitions: charge.scottish_alterations && charge.scottish_alterations.has_alterations_to_prohibitions ? charge.scottish_alterations.has_alterations_to_prohibitions : false,
-                chargeHasScottishAlterationsRestrictingProvisions: charge.scottish_alterations && charge.scottish_alterations.has_restricting_provisions ? charge.scottish_alterations.has_restricting_provisions : false,
-                chargeAssetsCeasedReleased: charge.assets_ceased_released ? assets_ceased_released : null,
-                chargeClassificationType: charge.classification && charge.classification.type ? charge.classification.type : null,
-                chargeClassificationDescription: charge.classification && charge.classification.description ? charge.classification.description : null,
-                chargeSecuredType: charge.secured_details ? charge.secured_details.type : null,
-                chargeSecuredDescription: charge.secured_details ? charge.secured_details.description : null
+                }) || null,
+                chargeParticulars: charge.particulars?.description || null,
+                chargeContainsFixedCharge: charge.particulars?.contains_fixed_charge || false,
+                chargeContainsFloatingCharge: charge.particulars?.contains_floating_charge || false,
+                chargeContainsNegativePledge: charge.particulars?.contains_negative_pledge || false,
+                chargeFloatingChargeCoversAll: charge.particulars?.floating_charge_covers_all || false,
+                chargeChargorActingAsBareTrustee: charge.particulars?.chargor_acting_as_bare_trustee || false,
+                chargeHasScottishAlterationsToOrder: charge.scottish_alterations?.has_alterations_to_order || false,
+                chargeHasScottishAlterationsToProhibitions: charge.scottish_alterations?.has_alterations_to_prohibitions || false,
+                chargeHasScottishAlterationsRestrictingProvisions: charge.scottish_alterations?.has_restricting_provisions || false,
+                chargeAssetsCeasedReleased: charge.assets_ceased_released || null,
+                chargeClassificationType: charge.classification?.type || null,
+                chargeClassificationDescription: charge.classification?.description || null,
+                chargeSecuredType: charge.secured_details?.type || null,
+                chargeSecuredDescription: charge.secured_details?.description || null
             }
             return fields
         })
