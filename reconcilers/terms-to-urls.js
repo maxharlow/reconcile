a61f66c8cd6fa17cfb0690c299bd58fb8cab74a3
Max Harlow <maxharlow@gmail.com>
2021-10-04 14:10:15 +0100

    Add supplement and includeAll parameters to terms-to-urls reconciler

--- reconcilers/terms-to-urls.js
+++ reconcilers/terms-to-urls.js
@@ -4,6 +4,7 @@ function initialise(parameters, requestor, die) {
 
     const request = requestor(1, e => {
         const term = e.config.passthrough.term
+        if (e.response.status === 429) die('The rate limit has been reached')
         if (e.response.status >= 400) return `Received code ${e.response.status} for term "${term}"`
     })
 
@@ -16,7 +17,7 @@ function initialise(parameters, requestor, die) {
                 'User-Agent': 'Reconcile'
             },
             params: {
-                q: term
+                q: parameters.supplement ? `${parameters.supplement} ${term}` : term
             },
             passthrough: {
                 term
@@ -27,14 +28,14 @@ function initialise(parameters, requestor, die) {
     async function paginate(response, responses = []) {
         const document = Cheerio.load(response.data)
         const hasMorePages = document('[aria-label="Next page"]').length
-        if (hasMorePages) {
+        if (parameters.includeAll && hasMorePages) {
             const query = {
                 url: response.url,
                 headers: {
                     'User-Agent': 'Reconcile'
                 },
                 params: {
-                    q: response.passthrough.term,
+                    q: parameters.supplement ? `${parameters.supplement} ${term}` : response.passthrough.term,
                     start: (responses.length + 1) * 10
                 },
                 passthrough: {
@@ -50,7 +51,7 @@ function initialise(parameters, requestor, die) {
         const document = Cheerio.load(response.data)
         const results = document('#main > div > div > div > a[href^="/url"]')
         if (results.length === 0) {
-            const term = response.passthrough.term
+            const term = parameters.supplement ? `${parameters.supplement} ${term}` : response.passthrough.term
             throw new Error(`Could not find term "${term}"`)
         }
         return results.get().flatMap(result => {
@@ -78,7 +79,9 @@ function initialise(parameters, requestor, die) {
 
 const details = {
     parameters: [
-        { name: 'termField', description: 'Term column.' }
+        { name: 'termField', description: 'Term column.' },
+        { name: 'supplement', description: 'Extra terms to be included with the search. [optional]' },
+        { name: 'includeAll', description: 'Set true to include all URLs, instead of just the first. [optional, default is first page only]' }
     ],
     columns: [
         { name: 'resultTitle' },
