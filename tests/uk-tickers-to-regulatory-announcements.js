511ba5e6345948f55603aabbf5b8eaaf5031ef43
Max Harlow <maxharlow@gmail.com>
2023-01-11 17:42:39 +0000

    Update test

--- tests/uk-tickers-to-regulatory-announcements.js
+++ tests/uk-tickers-to-regulatory-announcements.js
@@ -3,11 +3,12 @@ import FSExtra from 'fs-extra'
 import Reconcile from '../reconcile.js'
 
 Ava('standard', async test => {
+    test.timeout(60 * 1000)
     const filename = './tests/data/uk-tickers.csv'
     const reconcillation = await Reconcile('uk-tickers-to-regulatory-announcements', filename, {
         tickerField: 'ticker',
         category: 'results',
-        maximumDate: '2022-01-01'
+        maximumDate: '2023-01-01'
     })
     const processing = await reconcillation.run()
     const results = await processing.flatten().toArray()
